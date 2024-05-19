b3f6a60097fc7b95487dad6a1d91a0211723a63a
ky0422 <ky0422a@gmail.com>
2024-05-19 22:15:49 +0900

    updated Opcode

---
+++ src/opcode.rs
@@ -0,0 +1,42 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[repr(u8)]
+#[non_exhaustive]
+pub enum Opcode {
+    // 0x0-
+    NOOP = 0x00,
+    PUSH = 0x01,
+    STORE = 0x02,
+    LOAD = 0x03,
+
+    // 0x1-
+    ADD = 0x10,
+    SUB = 0x11,
+    MUL = 0x12,
+    DIV = 0x13,
+    MOD = 0x14,
+
+    AND = 0x18,
+    OR = 0x19,
+    XOR = 0x1A,
+    NOT = 0x1B,
+    LSF = 0x1C,
+    RSF = 0x1D,
+
+    // 0x2-
+    EQ = 0x20,
+    GT = 0x21,
+    LT = 0x22,
+    GTE = 0x23,
+    LTE = 0x24,
+
+    PROC = 0x28,
+    CALL = 0x29,
+    RET = 0x2A,
+
+    JMP = 0x2C,
+    JIF = 0x2D,
+
+    // 0xF-
+    DBG = 0xFE,
+    EXIT = 0xFF,
+}
