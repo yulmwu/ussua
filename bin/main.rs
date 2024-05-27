28f0073a9cbd0967914ab763275c83e72db3be79
ky0422 <ky0422a@gmail.com>
2024-05-27 21:47:53 +0900

    updated VM

--- bin/main.rs
+++ bin/main.rs
@@ -1,6 +1,18 @@
-use ussua::opcode::Opcode;
+use ussua::{opcode::{Op, Opcode}, vm::Vm, Instructions};
 
 fn main() {
     let a = Opcode::try_from(0xFF).unwrap();
     println!("{:?}", a);
+
+    let instructions = [
+        Op::new(Opcode::PROC, Some(2)),
+        Op::new(Opcode::NOOP, None),
+        Op::new(Opcode::RET, None),
+        Op::new(Opcode::CALL, Some(0)),
+        Op::new(Opcode::NOOP, None),
+    ];
+    let instructions = Instructions(&instructions);
+
+    let mut vm = Vm::new(instructions);
+    vm.execute().unwrap();
 }
