269993088d3099d0ac02e18b8f2191e3e7c8efb1
ky0422 <ky0422a@gmail.com>
2024-06-14 23:30:17 +0900

    updated Debugger

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,23 +1,32 @@
 use std::fmt::Display;
 
+use crate::Pointer;
+
 #[derive(Debug)]
-pub enum DebugKind {
-    Info,
-    Error,
+pub enum DebugMessage<T: Display> {
+    InstructionTrace(T, Pointer),
+    DbgInstruction(T),
 }
 
 pub trait Debugger: Default {
-    fn debug<T: Display>(&self, kind: DebugKind, msg: T);
+    fn debug<T: Display>(&self, msg: DebugMessage<T>);
 }
 
 #[derive(Default)]
 pub struct DebugPrint;
 
 impl Debugger for DebugPrint {
-    fn debug<T: Display>(&self, kind: DebugKind, msg: T) {
-        match kind {
-            DebugKind::Info => println!("{msg}"),
-            DebugKind::Error => eprintln!("{msg}"),
+    fn debug<T: Display>(&self, msg: DebugMessage<T>) {
+        match msg {
+            DebugMessage::InstructionTrace(msg, ptr) => println!("Executing {msg}, Pointer: {ptr}"),
+            DebugMessage::DbgInstruction(msg) => println!("DBG instruction: {msg}"),
         }
     }
+}
+
+#[derive(Default)]
+pub struct DebugDisable;
+
+impl Debugger for DebugDisable {
+    fn debug<T: Display>(&self, _: DebugMessage<T>) {}
 }
