018955d1e70536e1da8b81e36746414d7d0a3241
ky0422 <ky0422a@gmail.com>
2024-06-23 00:03:20 +0900

    updated Bytecode

--- bin/main.rs
+++ bin/main.rs
@@ -16,7 +16,29 @@ fn main() {
         Op::new(Opcode::CALL, Some(0)),
         Op::new(Opcode::NOOP, None),
     ];
+
+    println!(
+        "{:?}",
+        instructions
+            .iter()
+            .map(|v| format!(
+                "0x{:02x}{}",
+                v.opcode as u8,
+                match v.operand {
+                    Some(operand) => format!(" 0x{:02x}", operand as u8),
+                    None => String::new(),
+                }
+            ))
+            .collect::<Vec<_>>()
+    );
+
     let instructions = Instructions(&instructions);
+
+    let arr = [0x28, 0x02, 0x00, 0x2a, 0x29, 0x00, 0x00];
+    let arr: &[u8] = &arr;
+    let instructions_from_arr: Instructions = Instructions::try_from(arr).unwrap();
+
+    assert_eq!(instructions, instructions_from_arr);
 
     let mut vm = Vm::<DebugPrint>::new(instructions);
     vm.execute().unwrap();
